12900ffa519a35fdd8ecf1ce2a31d88b93024ca2
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-07 10:50:11 -0600

    added post

--- src/app.js
+++ src/app.js
@@ -14,32 +14,41 @@ const morganOption = (NODE_ENV === 'production')
   : 'common';
 
 app.use(morgan(morganOption))
+app.use(express.json())
 app.use(helmet())
 app.use(cors())
+
+let data = fakeData;
 
 app.get('/teleprompt', (req, res) => {
   const query = req.query;
 
   if (query.text) {
-  let results = fakeData.find(text => text.title === query.text );
+  let results = data.find(text => text.title === query.text );
   res.json(results);
   }
 
   else {
-    let results = fakeData.find(text => text.title === 'dummyStore');
+    let results = data.find(text => text.title === 'dummyStore');
     res.json(results);
   }
 
 });
 
 app.get ('/textTitles', (req, res) => {
-  let titles = fakeData.map(text => text.title);
+  let titles = data.map(text => text.title);
   res.json(titles);
 })
 
 
-app.post('/', (req, res) => {
-  res.send('POST request received')
+app.post('/upload', (req, res) => {
+const {title} = req.body;
+let newText = req.body;
+delete newText.title;
+delete newText.numOfSections;
+newText.title = title;
+data.push(newText);
+res.send('added new text');
 })
 
 app.use(function errorHandler(error, req, res, next) {
