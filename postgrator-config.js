14eb60f7469e761371fd24747d34c34843a13057
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-08 13:46:54 -0600

    db

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL,
+  "connectionString": process.env.DB_URL || 'postgresql://anugrahlambogo@localhost/Line-Please',
 }
---
+++ seeds/seed.allText.sql
@@ -0,0 +1,3 @@
+INSERT INTO allText (id, title, content )
+VALUES
+    ('1', 'moment 4 life', 'i wish that I could have this moment for life')
