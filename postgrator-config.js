b2efa40fd31c07ef0799a3a3cbcd29b5859ed7d2
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-10 11:47:01 -0600

    port variable

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,4 +4,5 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": process.env.DATABASE_URL || 'postgresql://anugrahlambogo@localhost/Line-Please',
+  "ssl": !!process.env.SSL,
 }
