0ea72c42ee13e541b12be4bdb2b7c7b5cd84e9af
Anugrah Lambogo <lambogoai@gmail.com>
2020-01-10 10:51:42 -0600

    changed db url

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,5 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL || 'postgresql://anugrahlambogo@localhost/Line-Please',
+  "connectionString": process.env.DATABASE_URL || 'postgresql://anugrahlambogo@localhost/Line-Please',
 }
